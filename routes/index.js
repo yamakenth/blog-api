54ec1972e5d34766623c06c3d7cf6f866bdbea0c
Ken Yamada <yamakenth@gmail.com>
2022-03-03 18:31:15 +0700

    Remove boilerplate files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('hello world');
+});
+
+router.get('/again', function(req, res, next) {
+  res.send('hello world again');
 });
 
 module.exports = router;
