6afd70959a74662f341f383960aa7f492b91b169
Ken Yamada <yamakenth@gmail.com>
2022-03-03 18:54:46 +0700

    Redirect to api.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('hello world');
-});
-
-router.get('/again', function(req, res, next) {
-  res.send('hello world again');
+  res.redirect('/api');
 });
 
 module.exports = router;
