03bb1eb9ed44693e326ce16f05cbe5106f5f70fa
Ken Yamada <yamakenth@gmail.com>
2022-03-03 22:38:27 +0700

    Create controller to create a comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,10 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+var Comment = require('../models/comment');
 
 // post a comment 
-exports.comment_post = function(req, res) {
-  res.send('COMMENT POST');
-}
+exports.comment_post = [
+  body('text', 'Comment cannot be empty').trim().isLength({ min: 1 }).escape(),
+  body('author', 'Author cannot be empty').trim().isLength({ min: 1 }).escape(),
+  
+  (req, res) => {
+    const errors = validationResult(req);
+  
+    const comment = new Comment({
+      article: req.params.articleid,
+      text: req.body.text,
+      author: req.body.author
+    });
+  
+    if (!errors.isEmpty()) {
+      res.send({ errors: errors.array().map(errorObj => errorObj.msg) });
+    } else {
+      comment.save(err => {
+        if (err) res.status(400).json('Error: ' + err);
+        res.send(comment);
+      });
+    }
+  }
+];
 
 // get a list of comments by article id 
 exports.comment_list_get = function(req, res) {
