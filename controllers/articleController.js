8a40a320b13874911135fc3082d11643891f70ac
Ken Yamada <yamakenth@gmail.com>
2022-03-03 22:07:38 +0700

    Create controller to POST article

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,10 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+var Article = require('../models/article');
 
 // create an article 
-exports.article_post = function(req, res) {
-  res.send('ARTICLE POST');
-}
+exports.article_post = [
+  body('title', 'Title cannot be empty').trim().isLength({ min: 1 }).escape(),
+  body('text', 'Text cannot be empty').trim().isLength({ min: 1 }).escape(),
+  body('author', 'Author cannot be empty').trim().isLength({ min: 1 }).escape(),
+  body('published', 'Published must be a boolean').isBoolean(),
+
+  (req, res) => {
+    const errors = validationResult(req);
+
+    const article = new Article({
+      title: req.body.title,
+      text: req.body.text,
+      author: req.body.author,
+      published: req.body.published
+    });
+
+    if (!errors.isEmpty()) {
+        res.send({ errors: errors.array().map(errorObj => errorObj.msg) });
+    } else {
+      article.save(err => {
+        if (err) res.status(400).json('Error: ' + err);
+        res.send(article);
+      });
+    }
+  } 
+];
 
 // get list of articles 
 exports.article_list_get = function(req, res) {
