0107f452880b9f6b26f98ebdae3fa921f43aa94a
Ken Yamada <yamakenth@gmail.com>
2022-03-03 18:42:21 +0700

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
+
+// connection to MongoDB
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedtopology: true });
+var db = mongoose.connection;
+db.once('open', () => console.log('MongoDB connection established successfully'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
